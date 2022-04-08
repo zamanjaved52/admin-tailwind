34dfe542963b8fd4be83ec9d07fd9af09ab0d988
zamanjaved527 <zamanjaved527@gmail.com>
2022-04-08 11:33:26 +0500

    dddd

--- resources/views/admin/pages/annuity/lookup.blade.php
+++ resources/views/admin/pages/annuity/lookup.blade.php
@@ -9,7 +9,7 @@
         }
     </style>
 
-    <table id="vdatatable" class="display example">
+    <table id="" class="display example">
         <thead>
         <tr>
             <th data-column="Username" class="vertical-header-column"></th>
@@ -67,111 +67,111 @@
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PERSON_STATUS dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">NATIONALITY dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PEP_STATUS</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">MAIDEN_NAME</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PREFERRED_PAYMENT_MODE dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">MEASUREMENT dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">SUB_STANDARD</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">AGE_ADMITTED</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">DEATH_ADMITTED</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">DOD_NOTIFIED</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">DEATH_DATE</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">BIRTH_NATIONALITY dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">RESIDENCY dropdown</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">MARITAL_STATUS dropdown </th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">MOTHERS_NAME</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">FATHERS_NAME</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">SALUTATION</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">KNOWN_AS</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">CURRENT_ADDRESS drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">YRS_AT_ADDRESS</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PREVIOUS_ADDRESS drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">CONTACT_DETAILS drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PREFERRED_CONTACT_MODE drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">SEND_SMS</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">IDENTIFICATION drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">CURRENT_OCCUPATION drop</th>
             <td data-column="Balakumar">3</td>
         </tr>
         <tr>
-            <th data-column="Username" class="vertical-header-column">PERNO</th>
+            <th data-column="Username" class="vertical-header-column">PREVIOUS_OCCUPATION</th>
             <td data-column="Balakumar">3</td>
         </tr>
         </tbody>
