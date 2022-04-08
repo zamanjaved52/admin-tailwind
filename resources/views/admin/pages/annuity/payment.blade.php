667ccb574d13a8898bce91dd569b6df2e907fa1a
zamanjaved527 <zamanjaved527@gmail.com>
2022-04-08 10:18:47 +0500

    d

--- resources/views/admin/pages/annuity/payment.blade.php
+++ resources/views/admin/pages/annuity/payment.blade.php
@@ -4,12 +4,14 @@
     <table id="" class="example display" style="width:100%">
         <thead>
         <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Office</th>
-            <th>Age</th>
-            <th>Start date</th>
-            <th>Salary</th>
+            <th>InternalID</th>
+            <th>PID</th>
+            <th>PolicyNo</th>
+            <th>Receipt</th>
+            <th>PaymentAmount</th>
+            <th>NextDueDate</th>
+            <th>NextInstallment</th>
+            <th>PaymentDate</th>
         </tr>
         </thead>
         <tbody>
@@ -20,11 +22,15 @@
             <td>61</td>
             <td>2011/04/25</td>
             <td>$320,800</td>
+            <td>$320,800</td>
+            <td>$320,800</td>
         </tr>
         <tr>
             <td>Garrett Winters</td>
             <td>Accountant</td>
             <td>Tokyo</td>
+            <td>63</td>
+            <td>63</td>
             <td>63</td>
             <td>2011/07/25</td>
             <td>$170,750</td>
@@ -35,6 +41,8 @@
             <td>San Francisco</td>
             <td>66</td>
             <td>2009/01/12</td>
+            <td>2009/01/12</td>
+            <td>2009/01/12</td>
             <td>$86,000</td>
         </tr>
         <tr>
@@ -44,11 +52,15 @@
             <td>22</td>
             <td>2012/03/29</td>
             <td>$433,060</td>
+            <td>$433,060</td>
+            <td>$433,060</td>
         </tr>
         <tr>
             <td>Airi Satou</td>
             <td>Accountant</td>
             <td>Tokyo</td>
+            <td>33</td>
+            <td>33</td>
             <td>33</td>
             <td>2008/11/28</td>
             <td>$162,700</td>
@@ -57,11 +69,15 @@
             <td>Brielle Williamson</td>
             <td>Integration Specialist</td>
             <td>New York</td>
+            <td>New York</td>
+            <td>New York</td>
             <td>61</td>
             <td>2012/12/02</td>
             <td>$372,000</td>
         </tr>
         <tr>
+            <td>Herrod Chandler</td>
+            <td>Herrod Chandler</td>
             <td>Herrod Chandler</td>
             <td>Sales Assistant</td>
             <td>San Francisco</td>
@@ -75,23 +91,9 @@
             <td>Tokyo</td>
             <td>55</td>
             <td>2010/10/14</td>
+            <td>2010/10/14</td>
+            <td>2010/10/14</td>
             <td>$327,900</td>
-        </tr>
-        <tr>
-            <td>Colleen Hurst</td>
-            <td>Javascript Developer</td>
-            <td>San Francisco</td>
-            <td>39</td>
-            <td>2009/09/15</td>
-            <td>$205,500</td>
-        </tr>
-        <tr>
-            <td>Sonya Frost</td>
-            <td>Software Engineer</td>
-            <td>Edinburgh</td>
-            <td>23</td>
-            <td>2008/12/13</td>
-            <td>$103,600</td>
         </tr>
         </tbody>
     </table>
